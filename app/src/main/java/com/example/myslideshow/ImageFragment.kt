78e856a6c7e4f51215bb256ec1538ee41630bdbc
takumi314 <isospin_symmetry@me.com>
2020-01-19 23:30:28 +0900

    add setter/getter of Argument's value loaded from Bundle

--- app/src/main/java/com/example/myslideshow/ImageFragment.kt
+++ app/src/main/java/com/example/myslideshow/ImageFragment.kt
@@ -12,6 +12,7 @@ import android.view.ViewGroup
  */
 class ImageFragment : Fragment() {
 
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -20,5 +21,25 @@ class ImageFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_image, container, false)
     }
 
+    companion object {
+        fun newInstance(imageResourceId: Int) : ImageFragment {
+            val bundle = Bundle()
+            // 画像リソースのIDをBundleに格納する
+            bundle.putInt("IMG_RES_ID", imageResourceId)
+            val imageFragment = ImageFragment()
+            imageFragment.arguments = bundle    // Argumentに書き込む
+            return imageFragment
+        }
+    }
+
+    // Argmentから取り出した画像IDを保持する
+    private var imgResId: Int? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            imgResId = it.getInt("IMG_RES_ID")
+        }
+    }
 
 }
