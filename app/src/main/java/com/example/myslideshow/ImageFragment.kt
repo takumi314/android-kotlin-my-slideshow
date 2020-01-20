4db598e42037f895cbef94ebe6de1659790f5e80
takumi314 <isospin_symmetry@me.com>
2020-01-21 00:06:12 +0900

    set image resource id for ImageView

--- app/src/main/java/com/example/myslideshow/ImageFragment.kt
+++ app/src/main/java/com/example/myslideshow/ImageFragment.kt
@@ -6,12 +6,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_image.*
 
 /**
  * A simple [Fragment] subclass.
  */
 class ImageFragment : Fragment() {
-
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -19,6 +19,15 @@ class ImageFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_image, container, false)
+    }
+
+    // アクティビティの onCreate が完了した後にコールされる
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        // imgResIdに保持しておいた画像IDを使って画像を設定する
+        imgResId?.let {
+            imageView.setImageResource(it)
+        }
     }
 
     companion object {
